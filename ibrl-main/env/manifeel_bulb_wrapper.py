7a4d1b05f1ab476616b6893b3f0b6653ba28219b
Pranesh <praneshv37@gmail.com>
2026-04-28 19:00:37 -0400

    Fix OmegaConf

--- ibrl-main/env/manifeel_bulb_wrapper.py
+++ ibrl-main/env/manifeel_bulb_wrapper.py
@@ -108,14 +108,6 @@ class ManiFeelBulbVecEnv:
             cfg.task.env.use_tactile_field_obs = False
             cfg.task.env.use_shear_force = False
             cfg.task.env.use_gelsight = False
-            for key in [
-                "left_tactile_camera_taxim",
-                "right_tactile_camera_taxim",
-                "tactile_force_field_right",
-                "tactile_depth_right",
-            ]:
-                if key in cfg.task.env.obsDims:
-                    del cfg.task.env.obsDims[key]
             self._env = MultipleIsaacEnvWrapper(cfg)
         self._seed = int(seed)
         self._episode_reward = torch.zeros(self.num_envs, dtype=torch.float32, device=self.device)
